0faf4fef1c65a0a9029814f79df435cfb8af96df
Dave <kdwiley1979@gmail.com>
2024-08-15 20:24:22 -0500

    Called images from API 081524

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,33 +1,52 @@
-// Get the DOM elements
+// Get the DOM elements, Get the img element by its id - Line 68 in HTML
 const imgElement = document.getElementById("image-element");
+//console.log("Image element:", imgElement);
 
 // API URL
 const apiURL = "http://localhost:5678/api/works";
 
 // Function to fetch images
-function fetch(apiURL) {
+function fetchImages(apiURL) {
+  // Make the fetch call When you call fetch(apiURL), it returns a promise that resolves to a Response object.
 
-  fetch(apiURL) // Make the fetch call When you call fetch(apiURL), it returns a promise that resolves to a Response object.
-    .then((response) => response.json()) // Handle the response and convert it to JSON
-    .then((data) => {
-      console.log(data);
-      // Use the JSON data to get the image URL and display the image
-      const imageURL = data[2].imageUrl;
-      display_image(imageURL);
-    })
-    // Catch any errors and log them
-    .catch((error) => console.error("Error fetching image:", error));
+  //async function catchImages() {}
+  //const response = await fetch(apiURL);
+  //const json = await response.json();
+  //document.getElementById("image-element");
 
-  // Function to display images
-  function display_image(imageUrl) {
-    // Get the img element by its id
-    //const imgElement = document.getElementById("image-element");
-    // Set the src attribute to the imageUrl
-    imgElement.src = imageUrl;
+  return (
+    fetch(apiURL) //By adding return in front of fetch(apiURL), you are ensuring that the Promise returned by fetch is also returned from the fetchImages function.
+      .then((response) => {
+        console.log("Response object:", response);
+        // Handle the response and convert it to JSON
+        return response.json();
+      })
+
+      .then((data) => {
+        loopThroughImageURLs(data);
+        // Use the JSON data to get the image URL and display the image.  .then(data) is referring to the JSON data received from the previous promise
+        const imageURL = data[2].imageUrl;
+        displayImage(imageURL);
+      })
+
+      // Catch and log errors
+      .catch((error) => console.error("Error fetching images:", error))
+  );
+}
+//Loop
+function loopThroughImageURLs(data) {
+  for (let i = 0; i < data.length; i++) {
+    let imageURL = data[i].imageUrl; // Assume data[i] contains image URL directly
+    displayImage(imageURL);
   }
 }
 
-console.log(data);
+// Function to display images
+function displayImage(imageURL) {
+  imgElement.src = imageURL;
+}
+
+fetchImages(apiURL); // Fetch images when the DOM is fully loaded
 
 // Function to apply filter
 
@@ -36,5 +55,3 @@ console.log(data);
 // Event listener for login link
 
 // Event listener for login form submission
-
-// Fetch images when the DOM is fully loaded
